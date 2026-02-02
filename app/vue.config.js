6b3fa91c4fcb2d73cba4fc8744554637eb2ac3d3
ali <eshax@qq.com>
2026-02-02 21:04:11 +0800

    优化加载速度

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
@@ -7,5 +8,45 @@ module.exports = defineConfig({
     // 后端转发服务运行在 http://localhost:3001
     // 详情请参考 BACKEND_PROXY_README.md
     port: 8080 // Vue开发服务器端口
+  },
+  configureWebpack: {
+    // 代码分割配置
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: 20,
+        maxAsyncRequests: 20,
+        minSize: 20000,
+        cacheGroups: {
+          // 分割 Element Plus 为单独的 chunk
+          elementPlus: {
+            name: 'chunk-element-plus',
+            test: /[\\/]node_modules[\\/]element-plus[\\/]/,
+            priority: 20,
+            chunks: 'all'
+          },
+          // 分割其他第三方库
+          vendors: {
+            name: 'chunk-vendors',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial'
+          },
+          // 分割公共代码
+          common: {
+            name: 'chunk-common',
+            minChunks: 2,
+            priority: 5,
+            chunks: 'initial',
+            reuseExistingChunk: true
+          }
+        }
+      }
+    }
+  },
+  // 关闭 preload 和 prefetch，减少不必要的资源加载
+  chainWebpack: config => {
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
   }
 })
